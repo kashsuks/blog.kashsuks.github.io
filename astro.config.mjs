71eee4dcadfe8a796c0334ca828bafaf73c85c10
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2025-10-30 10:29:39 -0400

    removed url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'kashyap.dino.icu/blog',
 	integrations: [mdx(), sitemap()],
 	base: '/blog'
 });
